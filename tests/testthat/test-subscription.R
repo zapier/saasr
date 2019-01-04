484f0b0b73be6936e8347c291196899942db8ebd
statwonk <cpeter9@gmail.com>
2019-01-04 21:10:15 +0000

    Oops, need to update the corresponding test when we make API changes.

--- tests/testthat/test-subscription.R
+++ tests/testthat/test-subscription.R
@@ -1,7 +1,8 @@
 context("R/subscriptions.R")
 
 test_that("cohort_to_calendar returns a tibble", {
-  expect_is(cohort_to_calendar(rweibull(1, 1, 1)), c("tbl_df", "tbl", "data.frame"))
+  expect_is(cohort_to_calendar(rweibull(1, 1, 1), 1, 2),
+            c("tbl_df", "tbl", "data.frame"))
 })
 
 test_that("subscribers_by_day returns a tibble", {
